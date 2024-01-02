caa9bcb4f56e434ced625c6a45c0d39d8ea7ade4
sani_demo <sanit.itpath@gmail.com>
2024-01-02 20:32:33 +0530

    added HTMl2

---
+++ JS/custom.js
@@ -0,0 +1,13 @@
+$('#testimonial_section').owlCarousel({
+	items: 3,
+	loop: true,	
+	margin: 30,
+	nav: true,
+	dots: false	,
+	center:true
+    // autoplay:true,
+    // autoplaySpeed: 6000,
+    // autoplayTimeout: 6000,
+    // slideTransition: 'linear',
+
+})
